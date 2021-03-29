369ca5e2280df317e9fd2208744f2fcbe6da334c
fhn <fhn037@126.com>
2021-03-29 13:29:33 +0800

    :bug:fix bug in task2

--- DisassembleData/1900012739_asmdata.py
+++ DisassembleData/1900012739_asmdata.py
@@ -27,27 +27,8 @@ df1 = pd.DataFrame(res1)
 df1.to_excel(writer,
              'instructionCount', index=False, header=None)
 
-# --------任务2：指令参数------------
-param_dict = {}
+# --------任务2：为任务1添加分类标签------------
 param_alia = {4: '无参指令', 5: '单参指令', 6: '双参指令', 7: '多参指令'}
-with open('./asmData.txt', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        content = line.split()
-        if content[-1] == 'return;':
-            content = content[:-1]
-        l = len(content)
-        if l <= 3:
-            continue
-        elif l > 7:
-            l = 7
-        param_dict[param_alia[l]] = param_dict.get(param_alia[l], 0) + 1
-
-res2 = sorted(param_dict.items(), key=lambda x: x[1], reverse=True)
-df2 = pd.DataFrame(res2, columns=['指令类型', '指令总量'])
-df2.to_excel(writer,
-             'instructionType', index=False, header=None)
-
-# --------任务3：为任务1添加分类标签------------
 type_dict = {}
 with open('asmData.txt', 'r', encoding='utf-8') as f:
     for line in f.readlines():
@@ -61,6 +42,15 @@ with open('asmData.txt', 'r', encoding='utf-8') as f:
             l = 7
         type_dict[content[3]] = max([type_dict.get(content[3], 0), l])
 
+res2 = []
+for item in res1:
+    res2.append([item[0], param_alia[type_dict[item[0]]]])
+df2 = pd.DataFrame(res2)
+df2.to_excel(writer,
+             'instructionType', index=False, header=None)
+
+# --------任务3：汇总1和2------------
+
 res3 = []
 for item in res1:
     res3.append([item[0], item[1], param_alia[type_dict[item[0]]]])
@@ -68,12 +58,27 @@ df3 = pd.DataFrame(res3)
 df3.to_excel(writer, 'Summary', index=False, header=None)
 
 # -----------任务4：绘制图片------------------
+param_dict = {}
+with open('./asmData.txt', 'r', encoding='utf-8') as f:
+    for line in f.readlines():
+        content = line.split()
+        if content[-1] == 'return;':
+            content = content[:-1]
+        l = len(content)
+        if l <= 3:
+            continue
+        elif l > 7:
+            l = 7
+        param_dict[param_alia[l]] = param_dict.get(param_alia[l], 0) + 1
+
+res4 = sorted(param_dict.items(), key=lambda x: x[1], reverse=True)
+df4 = pd.DataFrame(res4, columns=['指令类型', '指令总量'])
+
 inst_dict4 = {}  # 每个类之下有多少种指令
 for item in type_dict.keys():
     tp = param_alia[type_dict[item]]
     inst_dict4[tp] = inst_dict4.get(tp, 0) + 1
 
-df4 = df2
 df4.insert(1, '指令种类数', [inst_dict4[x] for x in df4.iloc[:, 0]])
 print(df4)
 
