9d38771659009259e2824c8afb4d0c751c690ac7
fhn <fhn037@126.com>
2021-04-02 19:56:12 +0800

    :sparkles:task 4 finished

--- Visualization/4_multi.py
+++ Visualization/4_multi.py
@@ -1,11 +1,14 @@
+from os import name, system
 from pyecharts import options as opts
-from pyecharts.charts import Bar, Grid, Line, Scatter
+from pyecharts.charts.chart import Chart
+from pyecharts.globals import ChartType, SymbolType
+from pyecharts.charts import Bar, EffectScatter, Page, Geo, Tab
 import json
 
 with open("./assets/GDP.json", 'r', encoding='utf-8') as f:
     gdp_dict = json.load(f)
     x_data = list(gdp_dict.keys())
-    y1_data= list(gdp_dict.values())
+    y1_data = list(gdp_dict.values())
 with open("./assets/GDP_2020.json", 'r', encoding='utf-8') as f:
     gdp_dict = json.load(f)
     y2_data = list(gdp_dict.values())
@@ -32,7 +35,7 @@ bar = (
             type_="value",
             name="COVID-19 rate",
             min_=0,
-            max_=100,
+            max_=10,
             position="left",
             axisline_opts=opts.AxisLineOpts(
                 linestyle_opts=opts.LineStyleOpts(color="#3ba25f")
@@ -43,47 +46,86 @@ bar = (
             ),
         )
     )
-    # .extend_axis(
-    #     yaxis=opts.AxisOpts(
-    #         name="2020",
-    #         type_="value",
-    #         min_=0,
-    #         max_=22,
-    #         position="right",
-    #         offset=60,
-    #         axisline_opts=opts.AxisLineOpts(
-    #             linestyle_opts=opts.LineStyleOpts(color="#d14a61")
-    #         ),
-    #         axislabel_opts=opts.LabelOpts(
-    #             formatter="${value}"
-    #         ),
-    #     )
-    # )
     .set_global_opts(
         yaxis_opts=opts.AxisOpts(
             name="GDP",
             min_=0,
             max_=22,
             position="right",
-            offset=0,#Y 轴相对于默认位置的偏移，在相同的 position 上有多个 Y 轴的时候有用
+            offset=0,  # Y 轴相对于默认位置的偏移，在相同的 position 上有多个 Y 轴的时候有用
             axisline_opts=opts.AxisLineOpts(
                 linestyle_opts=opts.LineStyleOpts(color="#675bba")
             ),
             axislabel_opts={
-                "formatter":"${value} trillion",
-                "rotate":-10,
+                "formatter": "${value} trillion",
+                "rotate": -10,
             }
         ),
         xaxis_opts=opts.AxisOpts(
             # name="country",
-            axislabel_opts={"rotate":30}
+            axislabel_opts={"rotate": 30}
         ),
         title_opts=opts.TitleOpts(title="GDP Top10"),
-        tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),#提示框配置项
+        tooltip_opts=opts.TooltipOpts(
+            trigger="axis", axis_pointer_type="cross"),  # 提示框配置项
     )
 )
 
-bar.render('./out/grid_mult_yaxis.html')
+with open("./assets/pandemic.json", 'r', encoding='utf-8') as f:
+    cvd_dict = json.load(f)
+    y3_data = [("%.1f"%(100*x['confirmed']/x['population'])) for x in cvd_dict.values()]
+c = (
+    EffectScatter()
+    .add_xaxis(
+        x_data,
+    )
+    .add_yaxis(
+        "COVID-19 rate",
+        y3_data,
+        yaxis_index=1,
+        itemstyle_opts=opts.ItemStyleOpts(color='#3ba25f'),
+        symbol=SymbolType.DIAMOND
+    )
+)
 
-from os import system
-system("start chrome ./out/grid_mult_yaxis.html")
+bar.overlap(c)
+
+
+y4_data = [list((x[0], ("%1.f"%((float(x[1])-float(x[2]))*45+5)))) for x in zip(x_data, y1_data, y2_data)]
+geo = (
+    Geo()
+    .add_schema(maptype='world')
+    .add_coordinate_json(json_file='./assets/world_country.json')
+    .add(
+        "GDP decrement",
+        y4_data,
+        type_=ChartType.EFFECT_SCATTER,
+        is_selected=False,
+        symbol=SymbolType.ROUND_RECT
+    )
+    .add(
+        "Pandemic",
+        [list((x[0], ("%.1f"%(100*10*x[1]['confirmed']/x[1]['population'])))) for x in cvd_dict.items()], 
+        type_=ChartType.EFFECT_SCATTER,
+        symbol=SymbolType.DIAMOND
+    )
+    .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+    .set_global_opts(
+        visualmap_opts=opts.VisualMapOpts(),
+        title_opts=opts.TitleOpts(title="GDP decrement in these countries"),
+    )
+)
+
+page = (
+    Page(
+        page_title = "COVID-19 effecting GDP Top10",
+        layout=Page.SimplePageLayout
+    )
+    .add(
+        bar,
+        geo,
+    )
+    .render("./out/pandemic-gdp.html")
+)
+
+system("start chrome ./out/pandemic-gdp.html")
